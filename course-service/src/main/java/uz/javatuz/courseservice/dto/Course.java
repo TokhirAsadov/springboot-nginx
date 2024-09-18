e82a90cbecdebc2db9f57cc8160c27aec814feb4
Takhir <guvalakat1603@gmail.com>
2024-09-18 11:41:34 +0500

    Course dto

---
+++ course-service/src/main/java/uz/javatuz/courseservice/dto/Course.java
@@ -0,0 +1,15 @@
+package uz.javatuz.courseservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    private String courseId;
+    private String name;
+    private double price;
+}
